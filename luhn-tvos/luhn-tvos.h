7d1d5c45252611fa9facdcf0f63b646f22e7b18f
Luca Serpico <serpicoluca@gmail.com>
2016-10-08 14:59:50 +0100

    Add carthage support with multiple configuration targets

---
+++ luhn-tvos/luhn-tvos.h
@@ -0,0 +1,19 @@
+//
+//  luhn-tvos.h
+//  luhn-tvos
+//
+//  Created by Luca Serpico on 08/10/2016.
+//  Copyright © 2016 Luca Serpico. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for luhn-tvos.
+FOUNDATION_EXPORT double luhn_tvosVersionNumber;
+
+//! Project version string for luhn-tvos.
+FOUNDATION_EXPORT const unsigned char luhn_tvosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <luhn_tvos/PublicHeader.h>
+
+
---
+++ luhn-wos/luhn-wos.h
@@ -0,0 +1,19 @@
+//
+//  luhn-wos.h
+//  luhn-wos
+//
+//  Created by Luca Serpico on 08/10/2016.
+//  Copyright © 2016 Luca Serpico. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for luhn-wos.
+FOUNDATION_EXPORT double luhn_wosVersionNumber;
+
+//! Project version string for luhn-wos.
+FOUNDATION_EXPORT const unsigned char luhn_wosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <luhn_wos/PublicHeader.h>
+
+
