06f97a01b84412a68f05989fee2183fe2dd3e1fe
Luca Serpico <serpicoluca@gmail.com>
2016-10-08 11:00:50 +0100

    Convert the playground in framework

---
+++ LuhnTests/LuhnTests.swift
@@ -0,0 +1,13 @@
+//
+//  LuhnTests.swift
+//  LuhnTests
+//
+//  Created by Luca Serpico on 08/10/2016.
+//  Copyright © 2016 Luca Serpico. All rights reserved.
+//
+
+import XCTest
+@testable import Luhn
+
+class LuhnTests: XCTestCase {
+}
